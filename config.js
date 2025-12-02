2d6286a90e859aba5e8f163241849f6d602ff425
cristobalalfaro-cmd <cristobal.alfaro@gmail.com>
2025-12-02 18:53:36 -0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Set here the Web App URL from your Google Apps Script deployment.
-// Example:
-// const API_URL = "https://script.google.com/macros/s/AKfycbx.../exec";
-
-const API_URL = "https://script.google.com/macros/s/AKfycbxD8PgYtpi8XxJxHZSqJGJCSPh4Oiij2kCH4uYitraTz_BCpVCRXguOc5AUCJ8T9vd9XQ/exec";
